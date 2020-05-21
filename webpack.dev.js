0a44a90c04cda83235809cc8bb7dc5cfddd67a85
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-05-20 21:11:23 -0700

    feat: set up dev and prod modes

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/client/index.js',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
