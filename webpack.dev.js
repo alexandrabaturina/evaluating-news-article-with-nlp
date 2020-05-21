bce8670683a547ba644f7c4578b4f690f207be41
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-05-21 12:10:55 -0700

    feat: set up sass styles

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,6 +13,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
