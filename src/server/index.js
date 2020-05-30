f06696d0f048ec10f1f6c67f24f89120d528b49b
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-05-30 10:25:37 -0700

    chore: configure express

--- src/server/index.js
+++ src/server/index.js
@@ -1,29 +1,47 @@
-// var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-const dotenv = require('dotenv')
-dotenv.config()
+const path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const AYLIENTextAPI = require("aylien_textapi");
 const textapi = new AYLIENTextAPI({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
 });
 
+const PORT = 8081;
+
+// Require Express to run server and routes
 const app = express()
 
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Start up an instance of app
 app.use(express.static('dist'))
 
 // console.log(__dirname)
+
+// designates what port the app will listen to for incoming requests
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}`)
+})
+
+app.get('/test', function (req, res) {
+    res.send({ text: "hi" })
+})
 
 app.get('/', function (req, res) {
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+
